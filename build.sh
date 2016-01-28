8930d796ac70ca043635eda892b94c3cd4bdfdbb
Darwin Biler <buonzz@gmail.com>
2016-01-29 00:07:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 rm -rf /home/vagrant/Code/satis-laravel/web/dist
-mkdir /home/vagrant/Code/satis-laravel/web/dist 
+mkdir -p /home/vagrant/Code/satis-laravel/web/dist 
 php /home/vagrant/Code/satis-laravel/bin/satis build /home/vagrant/Code/satis-laravel/config.json /home/vagrant/Code/satis-laravel/web --no-interaction --skip-errors
